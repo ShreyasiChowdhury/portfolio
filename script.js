c2d8e6432887229c29ed61f5b083a952d189065f
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2023-07-25 12:56:48 +0530

    First Commit

--- script.js
+++ script.js
@@ -6,21 +6,17 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
-
-let arr1 = Array.from(sections);
-let arr2 = Array.from(navLinks);
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    arr1.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        console.log(id);
         if (top >= offset && top < offset + height) {
-            arr2.forEach(links => {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
@@ -33,4 +29,24 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbymL9ioXmRHM2RLRqW0ao4LFe-2G2m5Ftr7B5fenLG2QocQ5i0w81pXwZskRlpoHuPcyA/exec'
+const form = document.forms['submit-to-google-sheet']
+const success = document.getElementById('success');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            success.style.color = '#00abf0';
+            success.style.fontSize = '2rem';
+            success.style.fontWeight = '600';
+            success.innerHTML = "Thanks for your message!";
+            setTimeout(function() {
+                success.innerHTML = "";
+            }, 3000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
 
